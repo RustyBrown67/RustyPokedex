942b6094b26844bb8d2fc3c2674653d9284a2e10
RustyBrown67 <apple@ians-mbp-2>
2016-09-10 18:08:43 +0100

    Initial Commit

---
+++ RustyPokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RustyPokedex
+//
+//  Created by Russell Brown on 10/09/2016.
+//  Copyright © 2016 Russell Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
